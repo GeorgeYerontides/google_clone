edb43992538afa4abf411376415cc390a12ce812
George Yerontides <georgeyerontides@gmail.com>
2023-02-19 16:54:25 +0200

    Implemented data fetching with google api

--- components/SearchPage/SearchHeaderOptions.js
+++ components/SearchPage/SearchHeaderOptions.js
@@ -4,11 +4,11 @@ import { SearchIcon,PhotographIcon,VideoCameraIcon } from '@heroicons/react/soli
 import { useRouter } from 'next/router'
 const SearchHeaderOptions = () => {
   const router = useRouter();
+
   return (
     <div className='flex space-x-8 select-none w-full justify-center text-s, text-gray-700 lg:pl-52 lg:justify-start border-b'>
         <SearchHeaderOption title='All' icon={SearchIcon} selected={router.query.searchType === '' || !router.query.searchType} path={''}/>
         <SearchHeaderOption title='Images' icon={PhotographIcon} selected={router.query.searchType === 'image'} path={'image'}/>
-        <SearchHeaderOption title='Videos' icon={VideoCameraIcon} selected={router.query.searchType === 'videos'} path={'videos'}/>
     </div>
   )
 }
--- pages/search/index.js
+++ pages/search/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
 import React from 'react'
 import SearchHeader from '@/components/SearchPage/SearchHeader'
-const SearchPage = () => {
+import Response from '@/Response'
+const SearchPage = (props) => {
+console.log(props.results);
   return (
     <>
     <Head>
@@ -13,3 +15,25 @@ const SearchPage = () => {
 }
 
 export default SearchPage
+
+export async function getServerSideProps(context){
+  const mockData = true
+  if(mockData)
+  {
+    return {
+      props: {
+        results: Response 
+      }
+    }
+  }
+
+  const res =   await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && ('&searchType='+context.query.searchType) }`
+  )
+  const data = await res.json();
+  return {
+    props: {
+      results: data 
+    }
+  }
+}
